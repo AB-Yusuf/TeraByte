c52d231110ad5d894be92d8784181acff9c22ebb
Abu-Maestro <engryusufmayowa@gmail.com>
2019-08-19 10:29:25 +0100

    Second Commit Added a controller Model and View for Phones

---
+++ Controllers/PhonesController.cs
@@ -0,0 +1,20 @@
+﻿using GigaByte.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigaByte.Controllers
+{
+    public class PhonesController : Controller
+    {
+        // GET: Phones
+        public ActionResult Index()
+        {
+            var phones = new Phones() { Name = "Abu" };
+            
+            return View(phones);
+        }
+    }
+}
---
+++ Models/Phones.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigaByte.Models
+{
+    public class Phones
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
